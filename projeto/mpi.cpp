4e473712ea3872e0a530682f95d86627c1bfd3de
Luca Mizrahi <lucam1@al.insper.edu.br>
2024-11-01 08:05:20 -0400

    avançando no projeto

--- projeto/mpi.cpp
+++ projeto/mpi.cpp
@@ -5,7 +5,6 @@
 #include <mpi.h>
 #include <chrono>
 #include <functional>
-#include <numeric>
 
 std::vector<std::vector<int>> LerGrafo(const std::string& nomeArquivo, int& numVertices) {
     std::ifstream arquivo(nomeArquivo);
@@ -25,7 +24,7 @@ std::vector<std::vector<int>> LerGrafo(const std::string& nomeArquivo, int& numV
     return grafo;
 }
 
-void EncontrarCliqueLocal(const std::vector<std::vector<int>>& grafo, std::vector<int>& cliqueMaxima, int inicio, int fim) {
+void EncontrarCliqueLocal(const std::vector<std::vector<int>>& grafo, std::vector<int>& cliqueMaxima, int numVertices, int rank, int size) {
     std::vector<int> cliqueAtual;
 
     std::function<void(std::vector<int>&, std::vector<int>&)> EncontrarCliques;
@@ -54,12 +53,17 @@ void EncontrarCliqueLocal(const std::vector<std::vector<int>>& grafo, std::vecto
         }
     };
 
-    std::vector<int> candidatos;
-    for (int i = inicio; i < fim; ++i) {
-        candidatos.push_back(i);
+    for (int i = rank; i < numVertices; i += size) {
+        cliqueAtual.clear();
+        cliqueAtual.push_back(i);
+        std::vector<int> candidatos;
+        for (int j = i + 1; j < numVertices; ++j) {
+            if (grafo[i][j]) {
+                candidatos.push_back(j);
+            }
+        }
+        EncontrarCliques(cliqueAtual, candidatos);
     }
-
-    EncontrarCliques(cliqueAtual, candidatos);
 }
 
 int main(int argc, char* argv[]) {
@@ -77,10 +81,8 @@ int main(int argc, char* argv[]) {
         grafo = LerGrafo(nomeArquivo, numVertices);
     }
 
-    // Transmite o número de vértices para todos os processos
     MPI_Bcast(&numVertices, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Transmite a matriz de adjacência inteira
     if (rank != 0) {
         grafo.resize(numVertices, std::vector<int>(numVertices, 0));
     }
@@ -89,37 +91,24 @@ int main(int argc, char* argv[]) {
         MPI_Bcast(grafo[i].data(), numVertices, MPI_INT, 0, MPI_COMM_WORLD);
     }
 
-    // Calcula o intervalo de vértices para cada processo
-    int verticesPorProcesso = numVertices / size;
-    int inicio = rank * verticesPorProcesso;
-    int fim = (rank == size - 1) ? numVertices : inicio + verticesPorProcesso;
-
-    // Calcula a clique máxima local
     std::vector<int> cliqueMaximaLocal;
     auto start = std::chrono::high_resolution_clock::now();
-    EncontrarCliqueLocal(grafo, cliqueMaximaLocal, inicio, fim);
+    EncontrarCliqueLocal(grafo, cliqueMaximaLocal, numVertices, rank, size);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
 
     int tamanhoMaximoLocal = cliqueMaximaLocal.size();
     int tamanhoMaximoGlobal;
-
-    // Reduz para encontrar o tamanho máximo global
     MPI_Reduce(&tamanhoMaximoLocal, &tamanhoMaximoGlobal, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
 
+    if (tamanhoMaximoLocal == tamanhoMaximoGlobal) {
+        MPI_Send(cliqueMaximaLocal.data(), tamanhoMaximoLocal, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+
     if (rank == 0) {
-        std::vector<int> cliqueMaximaGlobal = cliqueMaximaLocal;
+        std::vector<int> cliqueMaximaGlobal(tamanhoMaximoGlobal);
+        MPI_Recv(cliqueMaximaGlobal.data(), tamanhoMaximoGlobal, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        // Recebe cliques de outros processos e determina a maior
-        for (int i = 1; i < size; ++i) {
-            std::vector<int> cliqueRecebida(tamanhoMaximoGlobal);
-            MPI_Recv(cliqueRecebida.data(), cliqueRecebida.size(), MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            if (cliqueRecebida.size() > cliqueMaximaGlobal.size()) {
-                cliqueMaximaGlobal = cliqueRecebida;
-            }
-        }
-
-        // Exibe a clique máxima global
         std::cout << "Tamanho da clique máxima: " << cliqueMaximaGlobal.size() << std::endl;
         std::cout << "Vértices da clique máxima: ";
         for (int v : cliqueMaximaGlobal) {
@@ -127,9 +116,6 @@ int main(int argc, char* argv[]) {
         }
         std::cout << std::endl;
         std::cout << "Tempo de execução: " << duration.count() << " segundos" << std::endl;
-    } else {
-        // Envia a clique máxima local para o processo principal
-        MPI_Send(cliqueMaximaLocal.data(), cliqueMaximaLocal.size(), MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
